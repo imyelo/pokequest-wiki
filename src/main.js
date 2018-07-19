5b2fe6c737b355118f7d0419e1f13185d1ebe814
yelo <zhihuzeye@gmail.com>
2018-07-19 23:14:42 +0800

    feat: add google analytics

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import 'normalize.css'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueAnalytics from 'vue-analytics'
 import AnchorPlugin from './plugins/anchor'
 import App from './App.vue'
 import router from './router'
 import Screen from './components/layout/Screen.vue'
 import Main from './components/layout/Main.vue'
 import Navbar from './components/layout/Navbar.vue'
+
+const GA_ID = process.env.VUE_APP_GA_ID
 
 Vue.config.productionTip = false
 
@@ -15,6 +18,12 @@ Vue.component('Main', Main)
 Vue.component('Navbar', Navbar)
 
 Vue.use(VueRouter)
+if (GA_ID) {
+  Vue.use(VueAnalytics, {
+    id: GA_ID,
+    router,
+  })
+}
 Vue.use(AnchorPlugin)
 
 new Vue({
