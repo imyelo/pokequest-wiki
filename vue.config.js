8173cb0c20745cf9cd61e3131982c1b0b9e9ac36
imyelo <zhihuzeye@gmail.com>
2018-07-21 17:43:58 +0800

    build: move all cacheable files into one directory

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
+const CACHEABLE_DIRECTORY = 'static'
+
 module.exports = {
+  assetsDir: CACHEABLE_DIRECTORY, // cacheable dir
+
   chainWebpack (config) {
     let images = config.module.rule('images')
 
@@ -16,10 +20,15 @@ module.exports = {
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
     iconPaths: {
-      favicon32: 'img/icons/favicon-32x32.png',
-      favicon16: 'img/icons/favicon-16x16.png',
-      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
-      msTileImage: 'img/icons/mstile-144x144.png',
+      favicon32: `${CACHEABLE_DIRECTORY}/img/icons/favicon-32x32.png`,
+      favicon16: `${CACHEABLE_DIRECTORY}/img/icons/favicon-16x16.png`,
+      appleTouchIcon: `${CACHEABLE_DIRECTORY}/img/icons/apple-touch-icon-152x152.png`,
+      msTileImage: `${CACHEABLE_DIRECTORY}/img/icons/mstile-144x144.png`,
+    },
+    workboxOptions: {
+      include: [/^static\/.*$/],
+      importWorkboxFrom: 'local',
+      importsDirectory: CACHEABLE_DIRECTORY,
     },
   }
 }
